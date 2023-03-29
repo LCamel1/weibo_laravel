df1b8ac8b3161437ff744891426fdf46a135b8ff
LCamel1 <mamdy112889@qq.com>
2023-03-29 23:54:20 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,6 @@
         你现在所看到的是<a href="#"></a>
       </p>
       <p>一切从这里开始</p>
-      <p><a href="#" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+      <p><a href="{{ route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a></p>
      </div>
 @endsection
