a8e3b8578a753ce76c3bd340e59f07ecfbd23468
LCamel1 <mamdy112889@qq.com>
2023-03-29 23:36:24 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
       <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
       <a href="#" target=_blank class="text-decoration-none">刻苦练习，每日精进</a>
       <div class="float-end">
-        <a href="/about" class="text-decoration-none">关于</a>
+        <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
       </div>
     </footer>
