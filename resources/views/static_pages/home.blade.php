e85c8d3fcccf08546257fc4b496a24d4e98f48f4
LCamel1 <mamdy112889@qq.com>
2023-03-29 23:26:44 +0000

    切割头部和尾部子视图

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
+
 @section('content')
-     <div class="jumbotron">
+     <div class="bg-light p-3 p-sm-5 rounded">
       <h1>Hello Laravel</h1>
       <p class="lead">
         你现在所看到的是<a href="#"></a>
