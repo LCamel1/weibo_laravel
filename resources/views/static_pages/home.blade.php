225fafae2dac1b00919edd4f342b93d24faaa22b
LCamel1 <mamdy112889@qq.com>
2023-03-29 10:44:05 +0000

    样式初始化及用webpack.mix解决浏览器缓存问题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
-     <h1>主页</h1>
+     <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是<a href="#"></a>
+      </p>
+      <p>一切从这里开始</p>
+      <p><a href="#" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+     </div>
 @endsection
