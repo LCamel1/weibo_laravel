563349e0e508209940495c8bee02f904eb39604f
vagrant <vagrant@homestead>
2023-04-03 17:39:06 +0000

    用户显示页面

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-      <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+      <img class="brand-icon" src="##">
       <a href="#" target=_blank class="text-decoration-none">刻苦练习，每日精进</a>
       <div class="float-end">
         <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
