49466d9bd2dfe9ce3517745b8b06449452124936
vagrant <vagrant@homestead>
2023-04-06 08:10:52 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,20 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    //
+    /**
+     * 注册页面
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * 个人中心
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user')); #compact 方法将用户对象 $user转化为一个关联数组
     }
 
+    /**
+     * 用户创建(注册操作)
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -33,5 +42,46 @@ class UsersController extends Controller
         Auth::login($user);//注册成功后自动登录
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程～');
         return redirect()->route('users.show', [$user]); # 注册成功重定向跳转show页面，等同于redire()->route('users.show', [$user->id])
+    }
+
+    /**
+     * 编辑用户页面
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 执行编辑操作
+     */
+    public function update(User $user, Request $request)
+    {
+         $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $updateData = array();
+        if ($request->name != $request->local_name) {
+            //判断修改的用户名是否已存在
+            if (User::where('name', '=', $request->name)->first()) {
+                session()->flash('danger', '修改失败，该用户名已存在！');
+                return redirect()->back()->withInput();//返回
+            }
+            $updateData['name'] = $request->name;
+        }
+        if ($request->password) {
+            $updateData['password'] = bcrypt($request->password);
+        }
+
+        if (!empty($updateData)) {
+            $user->update($updateData);
+            session()->flash('success', '个人资料更新成功！');
+            return redirect()->route('users.show', $user->id);
+        } else {
+            session()->flash('warning', '没有需要更新的数据，请先修改！');
+            return redirect()->back()->withInput();//返回
+        }
     }
 }
