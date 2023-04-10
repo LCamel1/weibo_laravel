de487748fcbdd187a5f2c61a9e1ca021bc2e7f22
vagrant <vagrant@homestead>
2023-04-10 07:47:59 +0000

    重置密码

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -13,7 +13,7 @@ use Carbon\Carbon;
 class PasswordController extends Controller
 {
     /**
-     * 密码重置
+     * 密码重置提交邮箱页面
      */
     public function showLinkRequestForm()
     {
@@ -54,6 +54,67 @@ class PasswordController extends Controller
         });
 
         session()->flash('success', '重置密码邮件已发送，请注意查收');
+        return redirect()->back();
+    }
+
+    /**
+     * 更新密码页面
+     */
+    public function showResetForm(Request $request)
+    {
+        $token = $request->route()->parameter('token');
+        return view('passwords.reset', compact('token'));
+    }
+
+    /**
+     * 执行密码修改
+     */
+    public function reset(Request $request)
+    {
+        //1.验证数据是否合规
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $email= $request->email;
+        $token = $request->token;
+
+        //找回密码链接的有效时间
+        $expires = 60 * 60;
+
+        //2.根据邮箱获取用户信息并判断用户是否存在
+         $user = User::where("email", $email)->first();
+         if (is_null($user)) {
+            session()->flash('danger', '邮箱未注册');
+            return  redirect()->back()->withInput();
+        }
+
+        //3.获取发送邮件时记录的重置信息(包括时间)
+        $record = (array) DB::table('password_resets')->where('email', $email)->first();
+        if ($record) {
+            //判断是否过期了
+            if (Carbon::parse($record['created_at'])->addSeconds($expires)->isPast()) {
+                session()->flash('danger', '链接已过期，请重新尝试！');
+                return redirect()->back();
+            }
+
+            //判断token令牌是否正确
+            if ( !Hash::check($token, $record['token'])) {
+                session()->flash('danger', '令牌错误');
+                return redirect()->back();
+            }
+
+            //执行更新
+            if ($user->update(['password' => bcrypt($request->password)])) {
+                session()->flash('success', '密码重置成功，请使用新密码登录');
+                return redirect()->route('login');
+            } else {
+                session()->flash('danger', '密码重置失败！');
+                return redirect()->back();
+            }
+        }
+        session()->flash('danger', '未找到重置记录');
         return redirect()->back();
     }
 }
